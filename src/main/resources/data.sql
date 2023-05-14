bee2c9f72659fec44bf8c252fa3a7d8ec7a03d5d
G1 <g1.tommy.jeon@gmail.com>
2023-05-15 00:22:25 +0900

    feat: Adds R2dbc Configurations

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into boards (title, content, updated_at, created_at) values ('Title 1', 'Content1', now(), now());
+insert into boards (title, content, updated_at, created_at) values ('Title 2', 'Content2', now(), now());
+insert into boards (title, content, updated_at, created_at) values ('Title 3', 'Content3', now(), now());
+
+insert into users (name, age, profile_picture_url, updated_at, created_at) values ('user1', 10, null, now(), now());
+insert into users (name, age, profile_picture_url, updated_at, created_at) values ('user2', 20, null, now(), now());
+insert into users (name, age, profile_picture_url, updated_at, created_at) values ('user3', 30, null, now(), now());
